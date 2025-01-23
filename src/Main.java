1b2f53e80abfa47a71f1a906dbb393ffa08bb9f2
giorgizena123 <zenaishvili621@gmail.com>
2025-01-23 22:56:17 +0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,58 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Writer writer1 = new Writer("George", "Orwell");
+        Writer writer2 = new Writer("J.K.", "Rowling");
+
+
+        Book book1 = new Book("1984", 328, "Dystopian", writer1);
+        Book book2 = new Book("Harry Potter and the Philosopher's Stone", 223, "Fantasy", writer2);
+
+
+        Book[] books = {book1, book2};
+
+
+        for (Book book : books) {
+            System.out.println(book);
+
+            Fraction fraction1 = new Fraction(1, 2);
+            Fraction fraction2 = new Fraction(2, 4);
+            Fraction fraction3 = new Fraction(3, 4);
+
+
+            Fraction[] fractions = {fraction1, fraction2, fraction3};
+
+            for (Fraction fraction : fractions) {
+                System.out.println(fraction);
+            }
+
+            int equalCount = 0;
+            for (int i = 0; i < fractions.length; i++) {
+                for (int j = i + 1; j < fractions.length; j++) {
+                    if (fractions[i].equals(fractions[j])) {
+                        equalCount++;
+                    }
+                }
+            }
+
+
+            System.out.println("Number of equal fractions: " + equalCount);
+
+            QuadraticEquation eq1 = new QuadraticEquation(1, -3, 2);
+            QuadraticEquation eq2 = new QuadraticEquation(1, -2, 1);
+            QuadraticEquation eq3 = new QuadraticEquation(1, -3, 2);
+
+
+            QuadraticEquation[] equations = {eq1, eq2, eq3};
+
+            for (int i = 0; i < equations.length; i++) {
+                for (int j = i + 1; j < equations.length; j++) {
+                    if (equations[i].equals(equations[j])) {
+                        equalCount++;
+                    }
+                }
+            }
+            System.out.println("Number of equal quadratic equations: " + equalCount);
         }
     }
 }
