e6139c431b520b22ce896d82dc88d88a68a9b3fa
giorgizena123 <zenaishvili621@gmail.com>
2025-01-23 22:56:32 +0400

    Update QuadraticEquation.java

--- src/QuadraticEquation.java
+++ src/QuadraticEquation.java
@@ -1,4 +1,39 @@
-package PACKAGE_NAME;
+import java.util.Objects;
 
-public class QuadraticEquation {
-}
+class QuadraticEquation {
+    private double a, b, c;
+    private double x1, x2;
+
+    public QuadraticEquation(double a, double b, double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+
+
+        double discriminant = b * b - 4 * a * c;
+        if (discriminant >= 0) {
+            x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+            x2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+        } else {
+            x1 = x2 = Double.NaN;
+        }
+    }
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        QuadraticEquation that = (QuadraticEquation) obj;
+        return Double.compare(that.x1, x1) == 0 && Double.compare(that.x2, x2) == 0;
+    }
+
+
+    @Override
+    public String toString() {
+        return "QuadraticEquation{a=" + a + ", b=" + b + ", c=" + c + ", x1=" + x1 + ", x2=" + x2 + "}";
+    }
+
+
+
+    }
