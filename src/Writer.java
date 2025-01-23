8cc66320b64b30a888172afe6e17c7fa31c50603
giorgizena123 <zenaishvili621@gmail.com>
2025-01-23 22:56:48 +0400

    Update Writer.java

--- src/Writer.java
+++ src/Writer.java
@@ -1,4 +1,34 @@
-package PACKAGE_NAME;
+import java.util.Objects;
 
-public class Writer {
+class Writer {
+    private String name;
+    private String surname;
+
+    public Writer(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Writer writer = (Writer) obj;
+        return Objects.equals(name, writer.name) && Objects.equals(surname, writer.surname);
+    }
+
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname);
+    }
+
+
+    @Override
+    public String toString() {
+        return "Writer{name='" + name + "', surname='" + surname + "'}";
+    }
 }
+
+
