30fb596ff088410c4a81f590d20bc78073c2e5f8
giorgizena123 <zenaishvili621@gmail.com>
2025-01-23 22:56:01 +0400

    Update Fraction.java

--- src/Fraction.java
+++ src/Fraction.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
+import java.util.Objects;
 
-public class Fraction {
+class Fraction {
+    private int numerator;
+    private int denominator;
+
+    public Fraction(int numerator, int denominator) {
+        if (denominator == 0) {
+            throw new ArithmeticException("Denominator cannot be zero");
+        }
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Fraction fraction = (Fraction) obj;
+        return numerator * fraction.denominator == denominator * fraction.numerator;
+    }
+
+    @Override
+    public String toString() {
+        return numerator + "/" + denominator;
+    }
 }
+
+
